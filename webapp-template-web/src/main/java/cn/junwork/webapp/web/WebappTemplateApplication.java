4c4143ce3d1dcaec445d89734f6edc24e1d81663
jun <coderjunjun@gmail.com>
2020-08-02 15:25:42 +0800

    初始提交（完成maven项目结构）

---
+++ webapp-template-web/src/main/java/cn/junwork/webapp/web/WebappTemplateApplication.java
@@ -0,0 +1,13 @@
+package cn.junwork.webapp.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappTemplateApplication.class, args);
+	}
+
+}
---
+++ webapp-template-web/src/test/java/cn/junwork/webapp/WebappTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.junwork.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
