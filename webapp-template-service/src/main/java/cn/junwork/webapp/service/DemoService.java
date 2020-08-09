eb2ca1206b7f53026d2b0c58d931ac8387dd794e
jun <coderjunjun@gmail.com>
2020-08-09 20:16:04 +0800

    添加前端框架react

---
+++ webapp-template-service/src/main/java/cn/junwork/webapp/service/DemoService.java
@@ -0,0 +1,8 @@
+package cn.junwork.webapp.service;
+
+/**
+ * @author coderjunjun@gmail.com
+ * @date 2020/8/9
+ */
+public interface DemoService {
+}
--- webapp-template-web/src/main/java/cn/junwork/webapp/web/AppLauncher.java
+++ webapp-template-web/src/main/java/cn/junwork/webapp/web/AppLauncher.java
@@ -3,11 +3,15 @@ package cn.junwork.webapp.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
-public class WebappTemplateApplication {
+public class AppLauncher {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebappTemplateApplication.class, args);
+		log.info("开始启动...");
+		SpringApplication.run(AppLauncher.class, args);
 	}
 
 }
